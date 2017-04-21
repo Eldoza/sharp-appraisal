6d7b7bb359e84594fc50ab9a5df4c67202cd0d10
Chris Mendoza <cmend85@gmail.com>
2017-04-21 14:13:57 -0500

    Created static pages and basic design, implemented bootstrap

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,25 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def gallery
+  end
+
+  def home_owners
+  end
+
+  def lenders
+  end
+
+  def attorneys
+  end
+
+  def real_estate_agents
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+
+  root 'static_pages#home'
+  get 'static_pages/home'
+  get 'static_pages/about'
+  get 'static_pages/contact'
+  get 'static_pages/gallery'
+  get 'static_pages/home_owners'
+  get 'static_pages/lenders'
+  get 'static_pages/attorneys'
+  get 'static_pages/real_estate_agents'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
