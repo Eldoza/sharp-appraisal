72e6dfced5a0ce392b3f6d9080149b6a45b35fa2
Chris Mendoza <cmend85@gmail.com>
2017-04-26 21:19:10 -0500

    Removed mail to mailer gem and reconstructed mailer and recreated Contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,16 +1,3 @@
-class Contact < MailForm::Base
-  attribute :name,      :validate => true
-  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-  attribute :message
-  attribute :nickname,  :captcha  => true
-
-  # Declare the e-mail headers. It accepts anything the mail method
-  # in ActionMailer accepts.
-  def headers
-    {
-      :subject => "Potential Client Lead",
-      :to => "cmend85@gmail.com",
-      :from => %("#{name}" <#{email}>)
-    }
-  end
+class Contact < ApplicationRecord
+  
 end
